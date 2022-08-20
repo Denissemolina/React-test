1c86e0ed36c0a78119242d9c8e76644faa9e297b
DenisseMolina <dmolinaruelas@gmail.com>
2022-08-19 21:19:38 -0500

    Adding bootstrap

---
+++ react-test/src/App.js
@@ -0,0 +1,14 @@
+import Table from './Table';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Table />
+      </header>
+    </div>
+  );
+}
+
+export default App;
